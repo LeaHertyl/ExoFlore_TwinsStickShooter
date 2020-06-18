bf8c7997d36b9eca643b259de6ed77be19825a7a
Léa <lea.brechet@yahoo.fr>
2020-06-18 12:26:22 +0200

    Start PlayerScript + prefabs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject projectile;
+
+    private Vector2 direction;
+    private Rigidbody myRigidbody;
+    private PlayerBehaviour inputs;
+    private Camera mainCam;
+
     // Start is called before the first frame update
     void Start()
+    {
+        inputs = new PlayerBehaviour();
+        inputs.Enable();
+        inputs.Player.Move.performed += OnMovePerformed;
+        inputs.Player.Move.canceled += OnMoveCanceled;
+
+
+        //inputs.Player.Shoot.performed += OnShootPerformed;
+
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCam = Camera.main;
+    }
+    private void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMovePerformed(InputAction.CallbackContext obj)
     {
-        
+
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext obj)
+    {
+
     }
 }
