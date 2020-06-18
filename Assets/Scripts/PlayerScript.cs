5f1b664603ebfd2164a8b84ee5ead923ef5a7b92
Léa <lea.brechet@yahoo.fr>
2020-06-18 14:14:03 +0200

    Push modif

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,6 +13,7 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private PlayerBehaviour inputs;
 
+    private Camera mainCam;
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +27,20 @@ public class PlayerScript : MonoBehaviour
         //inputs.Player.Shoot.performed += OnShootPerformed;
 
         myRigidbody = GetComponent<Rigidbody2D>();
+        mainCam = Camera.main;
 
     }
     private void FixedUpdate()
     {
         myRigidbody.velocity = direction * (speed * Time.fixedDeltaTime);
+
+        ClampPosition();
     }
 
     private void OnMovePerformed(InputAction.CallbackContext obj)
     {
         direction = obj.ReadValue<Vector2>();
+        Debug.Log("salut toi !");
     }
 
     private void OnMoveCanceled(InputAction.CallbackContext obj)
@@ -43,4 +48,32 @@ public class PlayerScript : MonoBehaviour
         direction = Vector2.zero;
     }
 
+    private void ClampPosition()
+    {
+        // On récupère la distance entre la camera et le joueur
+        var zDistance = Mathf.Abs(transform.position.z - mainCam.transform.position.z);
+        // On assigne à cette variable la position dans le monde du coin inférieur gauche de la caméra
+        var leftBottomCorner = mainCam.ScreenToWorldPoint(new Vector3
+        {
+            x = 0,
+            y = 0,
+            z = zDistance
+        });
+        // On assigne à cette variable la position dans le monde du coin supérieur droit de la caméra
+        var rightTopCorner = mainCam.ScreenToWorldPoint(new Vector3
+        {
+            x = mainCam.pixelWidth,
+            y = mainCam.pixelHeight,
+            z = zDistance
+        });
+        var oldPosition = myRigidbody.position;
+        // On vient limiter la position en x et y pour qu’elle soit dans les limites du champ de vision de la caméra
+        var newPosition = new Vector3
+        {
+            x = Mathf.Clamp(oldPosition.x, leftBottomCorner.x, rightTopCorner.x),
+            y = Mathf.Clamp(oldPosition.y, leftBottomCorner.y, rightTopCorner.y),
+            //z = oldPosition.z
+        };
+        myRigidbody.position = newPosition;
+    }
 }
